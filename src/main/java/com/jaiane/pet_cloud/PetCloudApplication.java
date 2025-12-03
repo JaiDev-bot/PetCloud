f4624638b585e223d47b6451a2de88f8be698955
Jaiane <jaianesoares700@gmail.com>
2025-12-03 15:51:50 -0300

    feat: first commit

---
+++ src/main/java/com/jaiane/pet_cloud/PetCloudApplication.java
@@ -0,0 +1,13 @@
+package com.jaiane.pet_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaiane/pet_cloud/PetCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaiane.pet_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
