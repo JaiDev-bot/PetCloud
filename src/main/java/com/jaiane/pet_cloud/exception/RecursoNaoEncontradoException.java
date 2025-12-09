65384d88bb5bb7f6ce7cc5880d577acd34eef456
Jaiane <jaianesoares700@gmail.com>
2025-12-09 16:55:33 -0300

    feat(Exception): ✨️feito exceção personalizada

---
+++ src/main/java/com/jaiane/pet_cloud/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.jaiane.pet_cloud.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem){
+        super(mensagem);
+
+    }
+}
