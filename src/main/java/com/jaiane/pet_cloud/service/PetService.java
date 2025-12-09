160b2afc8d2a271746fb50a081984e53794d306f
Jaiane <jaianesoares700@gmail.com>
2025-12-09 15:41:42 -0300

    feat(Service): ✨️feito método de listagem

--- src/main/java/com/jaiane/pet_cloud/service/PetService.java
+++ src/main/java/com/jaiane/pet_cloud/service/PetService.java
@@ -8,10 +8,12 @@ import com.jaiane.pet_cloud.dto.PetRequestDto;
 import com.jaiane.pet_cloud.model.Pet;
 import com.jaiane.pet_cloud.repository.PetRepository;
 
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -46,6 +48,12 @@ public class PetService {
             throw  new RuntimeException("Erro ao fazer upload da imagem", e);
 
         }
+    }
+
+    public List<Pet> listar(){
+
+    return petRepository.findAll();
+
     }
 
     private String enviarImagemParaAzure(MultipartFile arquivo) throws IOException{
